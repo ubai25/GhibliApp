9ddcbccc42984eef1529bad765a7b34ac09f4666
Ubaidillah Ahmad <a.ubai25@gmail.com>
2021-07-06 20:41:21 +0700

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Ahmad Ubaidillah on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
